627fdc1ea1a4a95e815922fe546a9a423ee2c7c9
eleanorb19 <90636185+eleanorb19@users.noreply.github.com>
2022-04-14 14:48:14 +0100

    Merging Health and Demo

--- Formative_Script.R
+++ Formative_Script.R
@@ -1,8 +1,15 @@
 #######################FORMATIVE ASSESSMENT#####################################
 
+library(haven)
 
 London_Dist <- read.csv("London District codes.csv")
 London_Demo <- read.table("London ward data demographics.dat", header = TRUE, sep = '\t')
 London_Envi <- read.csv("London ward data environment.csv")
 London_Health <- read_sas("london ward data health.sas7bdat")
 London_socio <- read_sav("London ward data socioeconomic.sav")
+
+
+names(London_Demo)[names(London_Demo) == "ï..Wardname"] <- "Wardname"
+
+total <- merge(London_Health, London_Demo, 
+               by = "Wardname")
