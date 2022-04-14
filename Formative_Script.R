846d9e5e264ac289591e284022d4ecdcf593edae
eleanorb19 <90636185+eleanorb19@users.noreply.github.com>
2022-04-14 14:35:14 +0100

    Read in files

--- Formative_Script.R
+++ Formative_Script.R
@@ -1,1 +1,8 @@
 #######################FORMATIVE ASSESSMENT#####################################
+
+
+London_Dist <- read.csv("London District codes.csv")
+London_Demo <- read.table("London ward data demographics.dat", header = TRUE, sep = '\t')
+London_Envi <- read.csv("London ward data environment.csv")
+London_Health <- read_sas("london ward data health.sas7bdat")
+London_socio <- read_sav("London ward data socioeconomic.sav")
