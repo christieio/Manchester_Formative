715bccd3d0f9636ded06bff2f6a89933fad7c105
habp22 <90623610+habp22@users.noreply.github.com>
2022-04-14 14:59:42 +0100

    comments added

--- Formative_Script.R
+++ Formative_Script.R
@@ -9,16 +9,16 @@ London_Demo <- read.table("London ward data demographics.dat", header = TRUE, se
 London_Envi <- read.csv("London ward data environment.csv")
 London_Health <- read_sas("london ward data health.sas7bdat")
 London_socio <- read_sav("London ward data socioeconomic.sav")
+#2 have ward name, 2 have ward code, district just has district and district code. 
 
-
-
+#Change name of col for intuitiveness
 names(London_Demo)[names(London_Demo) == "ï..Wardname"] <- "Wardname"
 
+#Merge health and demo on wardname
 total <- merge(London_Health, London_Demo, 
                by = "Wardname")
 
-#2 have ward name, 2 have ward code, district just has district and district code. 
-
+#merge merged data above to environment on population census
 newmerge <- merge(total, London_Envi, 
                by = "Population2011Census")
 
