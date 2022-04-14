e9675bdb215f4736e6faf10917ea42960e2a2111
habp22 <90623610+habp22@users.noreply.github.com>
2022-04-14 14:50:38 +0100

    Comparing the datasets

--- Formative_Script.R
+++ Formative_Script.R
@@ -1,5 +1,8 @@
 #######################FORMATIVE ASSESSMENT#####################################
 
+install.packages("haven")
+library(haven)
+
 #Read in data sets
 London_Dist <- read.csv("London District codes.csv")
 London_Demo <- read.table("London ward data demographics.dat", header = TRUE, sep = '\t')
@@ -7,4 +10,10 @@ London_Envi <- read.csv("London ward data environment.csv")
 London_Health <- read_sas("london ward data health.sas7bdat")
 London_socio <- read_sav("London ward data socioeconomic.sav")
 #2 have ward name, 2 have ward code, district just has district and district code. 
+
+#Compare datasets
+install.packages("arsenal")
+library(arsenal)
+comparedf(London_Dist, London_Health)
+
 
