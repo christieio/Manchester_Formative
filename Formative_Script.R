c7b609fb6e32cf197b18d06638c10159dfb0c511
eleanorb19 <90636185+eleanorb19@users.noreply.github.com>
2022-04-15 18:34:15 +0100

    Attempting to split

--- Formative_Script.R
+++ Formative_Script.R
@@ -37,19 +37,26 @@ which(is.na(London_socio$hhSocialRented))
 London_socio <- London_socio[-c(622:657),]
 sum(is.na(London_socio))
 
+# Changing & to and
+London_Demo$Wardname <- gsub("&", "and", London_Demo$Wardname)
+
 # Extracting district code from Wardcode
 London_socio$Districtcode <- substr(London_socio$Wardcode, 0, 4)
+London_Envi$Districtcode <- substr(London_Envi$Wardcode, 0, 4)
 
 
 # Adding district name to health
 London_Health$District <- sapply(strsplit(London_Health$Wardname, "-", fixed = T), function(x) (x[1]))
+London_Demo$District <- sapply(strsplit(London_Demo$Wardname, "-", fixed = T), function(x) (x[1]))
 #Get rid of spaces after district 
 London_Health$District <- trimws(London_Health$District, "r")
+London_Demo$District <- trimws(London_Demo$District, "r")
 unique(London_Health$District)
+unique(London_Demo$District)
 
 #Merge health and demo on wardname
-total <- merge(London_Health, London_Demo, 
-               by = "Wardname", all = TRUE)
+Health_Demo <- merge(London_Health, London_Demo, 
+               by = c("Wardname", "District"), all = TRUE)
 
 # Merge Socio and Envi on Wardcode
 Socio_Envi <- merge(London_socio, London_Envi, 
@@ -58,6 +65,7 @@ Socio_Envi <- merge(London_socio, London_Envi,
 
 #Get rid of spaces before district code 
 London_Dist$Districtcode <- gsub('\\s+', '', London_Dist$Districtcode)
+London_Envi$Districtcode <- gsub('\\s+', '', London_Envi$Districtcode)
 London_Dist$Districtcode
 
 # Merging Socioeconomic, Environment and District codes by Districtcode
@@ -67,3 +75,15 @@ Merged <- merge(x = Socio_Envi, y = London_Dist, by = "Districtcode", all.x = TR
 All_data <- merge(x = Merged, y = total, by = c("District", "Population2011Census"), all = TRUE)
 
 
+# Trying to split data in Demo (THIS IS NOT FINISHED)
+London_Demo$Count <- str_count(London_Demo$Wardname, "\\w+")
+which(London_Demo$Count > 10)
+London_Error <- London_Demo[c(44,  48,  52,  77,  80,  90, 111, 126, 195, 275, 280, 297, 305),]
+
+sep <- separate(London_Error[1], col=Wardname, into=c('One', 'Two', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', 
+                                                      '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28',
+                                                      '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
+                                                      '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52',
+                                                      '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65'), sep='\n')
+sep <- str_split_fixed(London_Error$Wardname, "\n", n = 72)
+sep_2 <- str_split(sep[[1]], "\t")
